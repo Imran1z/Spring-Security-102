362813a45b8687357fe6a4c315ef9b82ecdc5b81
imran1z <shaikhzimran12334@gmail.com>
2026-01-10 20:15:40 +0530

    Security Filter chain

---
+++ src/main/java/com/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http){
+       return http.authorizeHttpRequests(req->req.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()).build();
+    }
+}
